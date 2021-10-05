586874c509207d43c8dbc204f1f14503ba8a2600
Sarah Hakani <sarahhakani@Sarahs-MBP.attlocal.net>
2021-10-05 00:37:19 -0400

    updates to prayers & prayer gallery p5 sketch

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 //adapted from Allison Parish's walk through here 
 // https://creative-coding.decontextualize.com/changes-over-time/
 
+
+// this is from me testing out why the backgrounds are keeping their history and drawing on top of each other! still haven't figured this out... 
 let origC1, origC2, origN, origNewC, c1, c2, n, newc, myCanvas;
 
 let prayerArray = [
@@ -8,10 +10,19 @@ let prayerArray = [
   "I pray that my grandma's flight goes well.",
   "I pray that I find a TV for cheap.",
   "I pray I find more time with my daughter.",
+  "I want to travel more!",
   "I pray that my mom's travels don't make her knee pain worse and she has peace of mind.",
   "My son just started a job, and I pray that he continues to feel motivation and joy in it.",
-  "I want to maintain my health for the rest of my life and stay out of the hospital."
+  "I want to maintain my health for the rest of my life and stay out of the hospital.",
+  "I want to go to sleep soon and stay asleep for a whole night!",
+  "I'm praying for unity in my family."
 ]
+
+
+// create global variables that allow me to make my random function move slower 
+let timeLastUpdated = Date.now()
+const TIME_BETWEEN_RANDOMIZATIONS = 1000; // milliseconds between new randoms
+
 
 function setup() {
   let myCanvas = createCanvas(window.innerWidth,window.innerHeight);
@@ -48,7 +59,20 @@ function draw() {
     }
   
   // About page & Submit page background 
+
   if (pageNumber == 0) {
+    clear();
+    c1 = color(255);
+    c2 = color(60, 40, 90);
+  
+    for(let y=0; y<height; y++){
+      n = map(y,0,height,0,1);
+      newc = lerpColor(c1,c2,n);
+      stroke(newc);
+      line(0,y,width, y);
+        } 
+    }
+  if (pageNumber == 1) {
     clear();
     c1 = color(255);
     c2 = color(30, 40, 90);
@@ -61,30 +85,29 @@ function draw() {
         } 
     }
 
-  if (pageNumber == 1) {
-    clear();
-    c1 = color(255);
-    c2 = color(60, 40, 90);
-  
-    for(let y=0; y<height; y++){
-      n = map(y,0,height,0,1);
-      newc = lerpColor(c1,c2,n);
-      stroke(newc);
-      line(0,y,width, y);
-        } 
-    }
+  // Prayer gallery sketch - This is not running as it is on the p5.js sketch 
+  // https://preview.p5js.org/shakani/present/C5dBiKZ6N
+  // Basically, the if statement is running one time (one prayer is displaying) - I think this is because its linked to us selecting button i=2 here - So the if within the if is making it run one time. Need to come back to this and debug. 
+  // Also want to play with animating the text to have it flowing more (time permitting)
 
-  // Prayer gallery sketch  
   if (pageNumber == 2) {
+    //Clearing the old gradient background 
+    //This clear doesn't seem to be clearing the other buttons, something about the gradient is building from each button.
     clear();
+
+    // Soothing color 
+    background("lavender");
+
     let arrayLength = prayerArray.length;
     let randomPrayer = (Math.floor(random(0,arrayLength)));
-    //checking to see if random prayer is working as the draw loop progresses 
-    // console.log(prayerArray[randomPrayer])
-    textSize(24);
-    fill(random(80),random(80),random(200), 70);
-    text(prayerArray[randomPrayer], random(window.innerWidth), random(window.innerHeight));
     
+    textSize(24);
+    noStroke();
+    fill(random(80),random(80),random(200), 70);
+    if (Date.now() - timeLastUpdated > TIME_BETWEEN_RANDOMIZATIONS) {
+      text(prayerArray[randomPrayer], random(window.innerWidth), random(window.innerHeight));
+    timeLastUpdated = Data.now();
+    }
 
 
     }
